3ed8e87ed98cd20f062f0467bff43fe0bf03b31d
Paweł Pacana <pawel.pacana@gmail.com>
2021-12-15 02:42:09 +0100

    Ruby 2-3 compatibility layer

--- lib/constructor.rb
+++ lib/constructor.rb
@@ -2,4 +2,7 @@ module Constructor
   def new(*)
     super.tap { |instance| instance.instance_variable_set(:@version, -1) }
   end
+
+  # https://eregon.me/blog/2021/02/13/correct-delegation-in-ruby-2-27-3.html
+  ruby2_keywords :new if respond_to?(:ruby2_keywords, true)
 end
